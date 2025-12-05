82bb04a08fab4dd0a87c8ff97063406e7cd4d156
Abcent1k <snakyizze@gmail.com>
2025-12-05 18:17:38 +0200

    feature: create DI extension method

---
+++ Cinema/Services/Bookings/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,121 @@
+namespace Infrastructure;
+
+using System.Security.Cryptography.X509Certificates;
+using Application.Abstractions.Repositories;
+using Application.Abstractions.Services;
+using Application.Options;
+using Database;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Repositories;
+using Services;
+using Showtime.Grpc;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration) =>
+        services
+            .AddDatabase(configuration)
+            .AddGrpcClients(configuration)
+            .AddRepositories()
+            .AddAuthOptions(configuration)
+            .AddAuthenticationInternal()
+            .AddAuthorizationInternal();
+
+    private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(connectionString)
+                .UseSnakeCaseNamingConvention());
+
+        return services;
+    }
+
+    private static IServiceCollection AddAuthOptions(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<JwtOptions>(configuration.GetSection("Jwt"));
+
+        return services;
+    }
+
+    private static IServiceCollection AddGrpcClients(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddGrpcClient<ShowtimeService.ShowtimeServiceClient>(options =>
+        {
+            options.Address = new Uri(configuration["Grpc:ShowtimeServiceUrl"]
+                                      ?? throw new InvalidOperationException());
+        });
+
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IBookingRepository, BookingRepository>();
+        services.AddScoped<IUnitOfWork, EfUnitOfWork>();
+        services.AddScoped<IShowtimeReadService, ShowtimeReadService>();
+        services.AddScoped<IUserContextService, UserContextService>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddAuthenticationInternal(this IServiceCollection services)
+    {
+        services.AddSingleton<X509Certificate2>(sp =>
+        {
+            var jwtOptions = sp.GetRequiredService<IOptions<JwtOptions>>().Value;
+            return X509CertificateLoader.LoadCertificateFromFile(jwtOptions.SigningCertificatePath);
+        });
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer();
+
+        services.AddOptions<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme)
+            .Configure<IOptions<JwtOptions>, X509Certificate2>((options, jwtOptionsAccessor, cert) =>
+            {
+                var jwt = jwtOptionsAccessor.Value;
+
+                options.RequireHttpsMetadata = true;
+                options.SaveToken = true;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = jwt.Issuer,
+                    ValidAudience = jwt.Audience,
+                    IssuerSigningKey = new X509SecurityKey(cert),
+
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                };
+
+                options.Events = new JwtBearerEvents();
+            });
+
+        return services;
+    }
+
+    private static IServiceCollection AddAuthorizationInternal(this IServiceCollection services)
+    {
+        services.AddAuthorizationBuilder()
+            .AddPolicy("AdminPolicy", policy => { policy.RequireRole("Admin"); });
+
+        return services;
+    }
+}
